1da32ba4dfab74039b072eb4eb474477a43b3ade
jonjpbm <jonjpbm@users.noreply.github.com>
2020-04-03 11:56:05 -0500

    Update PowerShellTemplate.ps1

--- PowerShellTemplate.ps1
+++ PowerShellTemplate.ps1
@@ -1,130 +1,119 @@
 <#
 .SYNOPSIS
-  <Overview of script>
+    <Overview of script>
 .DESCRIPTION
-  <Brief description of script>
+    <Brief description of script>
 .PARAMETER <Parameter_Name>
-  <Brief description of parameter input required. Repeat this attribute if required>
+    <Brief description of parameter input required. Repeat this attribute if required>
 .INPUTS
-  <Inputs if any, otherwise state None>
+    <Inputs if any, otherwise state None>
 .OUTPUTS
-  <Outputs if any, otherwise state None>
+    <Outputs if any, otherwise state None>
 .NOTES
-  Version:        1.0
-  Author:         <Name>
-  Creation Date:  <Date>
-  Purpose/Change: Initial script development
+    Version:        1.0
+    Author:         <Name>
+    Creation Date:  <Date>
+    Purpose/Change: Initial script development
 .EXAMPLE
-  <Example explanation goes here>
+    <Example explanation goes here>
 
-  <Example goes here. Repeat this attribute for more than one example>
+    <Example goes here. Repeat this attribute for more than one example>
 #>
-
 #---------------------------------------------------------[Script Parameters]------------------------------------------------------
-
 [CmdletBinding()]
 Param (
-  #Script parameters go here
-  [Parameter(Mandatory = $false)] [String] $Path
-  ,[Parameter(mandatory=$false)] [switch]$RunningLog
+    #Script parameters go here
+    [Parameter(mandatory=$false)] [switch]$RunningLog
+    ,[Parameter(mandatory=$false)] [string]$LogDirectory
+    ,[Parameter(mandatory=$false)] [switch]$NoLog
 )
-
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
-
 #Set Error Action to Silently Continue
-#$ErrorActionPreference = 'Stop'
 
 #Import Modules & Snap-ins
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
-
 #Any Global Declarations go here
 $computerName = $env:COMPUTERNAME
-$ScriptRoot=$PSScriptRoot
-$ScriptLogTime = Get-Date -format "yyyyMMddmmss"
-$ScriptName = (Get-Item $PSCommandPath).Basename
-$LogDirectory = $ScriptRoot
+$script = $MyInvocation.MyCommand.Name
+$scriptName = [IO.Path]::GetFileNameWithoutExtension($Script)
+$ScriptLogTime = Get-Date -format "yyyyMMddHHmmss"
 $PSVersionReturned=$PSVersionTable.PSVersion
 $Date = Get-Date
-
-if($RunningLog){
-  Write-Verbose "Running Log switch passed"
-  $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
-}else {
-  $ScriptLog= "$LogDirectory\$ScriptName.log"
+if ($NoLog){
+    #Do not create a log file
+    Write-Verbose "No log file switch passed"
+    Write-Verbose "Getting PWD"
+    $PresentWorkingDirectory = Get-Location
+}Else{
+    Write-Verbose "Getting PWD"
+    $PresentWorkingDirectory = Get-Location
+    #Did you pass your own directory for the log?
+    if($PSBoundParameters.ContainsKey('LogDirectory')){
+        Write-Verbose "Log Directory Passed"
+        Write-Verbose $LogDirectory
+    }Else{
+        $LogDirectory = $PresentWorkingDirectory
+    }
+    #Create a unique log file
+    if($RunningLog){
+        Write-Verbose "Running Log switch passed"
+        $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
+    }else {
+        $ScriptLog= "$LogDirectory\$ScriptName.log"
+    }
 }
-
-
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 function Script_Information {
-    param (
-  )
-    Write-Output "$($MyInvocation.MyCommand)"
-    $Date.DateTime
-    Write-Output "Computer Name: $computerName"
-    Write-Output "PowerShell Version $PSVersionReturned"
-    Write-Output "ScriptRoot: $ScriptRoot"
-    Write-Output "ScriptName: $ScriptName"
-    Write-output "ScriptLog: $ScriptLog"
-
-}
-
-function Test_FileLock {
-  param ([parameter(Mandatory=$true)][string]$Path)
-  Write-Output "$($MyInvocation.MyCommand)"
-  $oFile = New-Object System.IO.FileInfo $Path
-  if ((Test-Path -Path $Path) -eq $false){
-    Write-Verbose "Default log file does not exist"
-    Return 0
-  }else{
-    try{
-      $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
-      if ($oStream){
-        $oStream.Close()
-      }
-      Return 0
-    }catch{
-      # file is locked by a process.
-      return 1
+    param ()
+    Write-Verbose "Function: $($MyInvocation.MyCommand)"
+    Write-Verbose $Date.DateTime
+    Write-Verbose "Computer Name: $computerName"
+    Write-Verbose "PowerShell Version $PSVersionReturned"
+    Write-Verbose "ScriptName: $ScriptName"
+    Write-Verbose "Present Working Directory: $PresentWorkingDirectory"
+    if($False -eq $NoLog){
+        Write-Verbose "ScriptLog: $ScriptLog"
     }
-  }
 }
-#-----------------------------------------------------------[End Of Functions]------------------------------------------------------------
-
 #-----------------------------------------------------------[Main]------------------------------------------------------------
 Function Main {
-  Param ()
-  Begin {
-    Write-Output "$($MyInvocation.MyCommand)"
-    Write-Output '<description of what is going on>...'
-    $StopWatch = [System.diagnostics.stopwatch]::StartNew()
-    Script_Information
-  }
-  Process {
-    Try {
-      Write-Output "Code Goes Here"
+    Param ()
+    Begin {
+        Write-Verbose "Function: $($MyInvocation.MyCommand)"
+        Write-Verbose 'Starting'
+        $StopWatch = [System.diagnostics.stopwatch]::StartNew()
+        Script_Information
     }
-    Catch {
-        Write-Error $PSItem -ErrorAction Stop
+    Process {
+        Try {
+            #code goes here
+        }
+        Catch {
+            Write-Error $PSItem
+        }
     }
-  }
-  End {
-    If ($?) {
-      write-output 'Completed Successfully.'
-      $StopWatch.Stop()
-      Write-Output "Elapsed Seconds $($StopWatch.Elapsed.TotalSeconds)"
+    End {
+        If ($?) {
+            Write-Verbose 'Completed Successfully.'
+            $StopWatch.Stop()
+            Write-Verbose "Elapsed Seconds $($StopWatch.Elapsed.TotalSeconds)"
+        }
     }
-  }
 }
-#-----------------------------------------------------------[End Of Main]------------------------------------------------------------
-
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 #Call main
-$IsFileLocked=Test_Filelock($Scriptlog)
-Write-verbose "IsFileLocked: $IsFileLocked"
-if($IsFileLocked -eq 1){
-  Write-Verbose "Default log file in use"
-  $ScriptLog= "$LogDirectory\$ScriptName`_$ScriptLogTime.log"
+if($NoLog){
+    Main *>&1
+}Else{
+    if ($False -eq $(test-path -PathType Container -path $LogDirectory)){
+        Write-Verbose "Attempting to create log directory"
+        try {
+            New-Item -ItemType Directory -Force -Path $LogDirectory
+        }
+        catch {
+            Write-Error $PSItem -ErrorAction Stop
+        }
+    }
+    Main *>&1 | Tee-Object $ScriptLog
 }
-Main *>&1 | Tee-Object $ScriptLog
-#-----------------------------------------------------------[End Of Execution]------------------------------------------------------------
